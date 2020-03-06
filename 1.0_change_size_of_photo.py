8468a58524c0e04329f94953047f9615412d87da
Ildaron <de.ikar@yandex.ru>
2020-03-06 14:22:48 +0900

    Update 1.0_change_size_of_photo.py

--- 1.0_change_size_of_photo.py
+++ 1.0_change_size_of_photo.py
@@ -1,9 +1,6 @@
 import cv2
 import numpy as np
-
-#predict_datagen = ImageDataGenerator(rescale=1./255)
-
-img1 = cv2.imread('path')
+img1 = cv2.imread('path.jpg')
 cv2.imshow('ImageWindow', img1)
 img1 = cv2.resize(img1, (64, 64))
 cv2.imshow('ImageWindow1', img1) 
