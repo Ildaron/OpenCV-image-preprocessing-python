adcf91f7561cf9ed75df52bebe0845a2ddc2807f
Ildaron <de.ikar@yandex.ru>
2022-04-30 23:14:10 +0100

    Update 3.In_range.py

--- laser_beam/3.In_range.py
+++ laser_beam/3.In_range.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np
 import imutils
 from itertools import product
+import math
 
-image = cv2.imread("data_44.jpg")
+image = cv2.imread("data_6.jpg")
 scale_percent = 40
 x = int(image.shape[1] * scale_percent / 100)
 y = int(image.shape[0] * scale_percent / 100)
@@ -41,31 +42,32 @@ else:
   y_max.append(y)
  index = radius_max.index(max(radius_max))
 
-
- def points_in_circle(radius):
-   
-  for x, y in product(range(int(radius) + 1), repeat=2):
-   if x**2 + y**2 <= radius**2:
-    yield from set(((x, y), (x, -y), (-x, y), (-x, -y),))
- print (list(points_in_circle(radius_max[1])))
- #points_in_circle(radius_max[1])
- 
+ print ("image.shape", image.shape)
+ print ("image.shape", image.shape[0])
+ print ("image.shape", image.shape[1])
+      
  if radius > 0:  
   cv2.circle(images, (int(x_max[index]), int(y_max[index])), int(radius_max[index]),(100, 0, 255), 2)
   cv2.circle(image, (int(x_max[index]), int(y_max[index])), int(radius_max[index]),(100, 0, 255), 2)
-  for a in list(points_in_circle(radius_max[1])):
-   #count = cv2.countNonZero
-
-   a_x=a[0]+x_max[1]
-   a_y = a[1]+y_max[1] 
-   print ("ok",image[int(a_x),int(a_y)])
-  
-  
   b=((amount_figure.index(max(amount_figure))))
-   #cv2.putText(image, 'object was found', (int (amount_axeX[b]) , int (amount_axeY[b])), cv2.FONT_ITALIC, 0.4, 255) # # cv2.putText(frame,text,location,font,font size,font color, font weight, line)
+
+  white_color=0
+  black_color=0
+  for a_x in range(0,image.shape[0]+1,1):
+   for a_y in range(0,image.shape[1]+1,1):   
+    distance = math.sqrt((a_x - x_max[index])**2 + (a_y - y_max[index])**2)    
+    if radius_max[index]>=distance:
+     #cv2.imshow("Frame", images)
+     #key = cv2.waitKey(1) & 0xFF 
+     if image[a_y,a_x]>200:
+      white_color=white_color+1
+     else:
+      black_color=black_color+1
+  circle=white_color+black_color
+  percentage=100*white_color/circle
+  print("percentage",percentage)
+  
+ #cv2.putText(image, 'object was found', (int (amount_axeX[b]) , int (amount_axeY[b])), cv2.FONT_ITALIC, 0.4, 255) # # cv2.putText(frame,text,location,font,font size,font color, font weight, line)
   cv2.imshow("Frame", images)
   cv2.imshow('Vertical', image)
   key = cv2.waitKey(1) & 0xFF
-
-
-   
