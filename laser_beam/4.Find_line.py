ae8fe63e2713e9a17474a95c8bc9459e68c42bef
Ildaron <de.ikar@yandex.ru>
2022-05-19 10:55:37 +0100

    Create 4.Find_line.py

---
+++ laser_beam/4.Find_line.py
@@ -0,0 +1,59 @@
+import cv2 
+import numpy as np
+from matplotlib import pyplot as plt
+import math
+
+img = cv2.imread("img.jpg")
+resize=100 #%
+x = int(img.shape[1] * resize / 100)
+y = int(img.shape[0] * resize / 100)
+source=img=cv2.resize(img,(x,y))
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV )
+h_min = np.array((42, 28, 64), np.uint8)
+h_max = np.array((151, 255, 237), np.uint8)
+img = cv2.inRange(img, h_min, h_max)
+#img = cv2.erode(img, None, iterations=5)
+#img = cv2.dilate(img, None, iterations=5)
+
+edges=img
+edges = cv2.Canny(img,100,200, L2gradient = True)
+#ret,edges = cv2.threshold(edges,200,250,cv2.THRESH_BINARY) #cv2.THRESH_BINARY_INV,cv2.THRESH_TRUNC,cv2.THRESH_TOZERO,cv2.THRESH_TOZERO_INV
+
+#cv2.imshow("Frame", edges)
+plt.imshow(edges)
+plt.show()
+
+contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+for i, c in enumerate(contours):
+  area = cv2.contourArea(c)
+  img=cv2.drawContours(source, contours, i, (78, 90, 55), 1)
+
+
+rho = 1 # distance resolution in pixels of the Hough grid
+theta = np.pi / 250  # angular resolution in radians of the Hough grid
+threshold = 50  # 15 minimum number of votes (intersections in Hough grid cell)
+min_line_length = 60  # 150 minimum number of pixels making up a line
+max_line_gap = 10  # maximum gap in pixels between connectable line segments
+
+lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),min_line_length, max_line_gap)
+number =0
+
+
+for line in lines:
+    #print (line)  
+    for x1,y1,x2,y2 in line:
+     if math.hypot(x2-x1, y2-y1)>10:
+      cv2.line(source,(x1,y1),(x2,y2),(255,0,0),1)
+      cv2.line(source,(x2,y2-100),(x2,y2),(15,10,0),1)      
+      ba=np.array([x1,y1])-np.array([x2,y2])
+      bc =np.array([x2,y2+100]) - np.array([x2,y2])
+      cosine_angle = np.dot(ba,bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+      angle = np.arccos(cosine_angle) 
+      pAngle = np.degrees(angle)
+      print (pAngle)
+      cv2.putText(source, str(round(pAngle,1)), (int (x1) , int (y1)), cv2.FONT_ITALIC, 0.4, 255) # # cv2.putText(frame,text,location,font,font size,font color, font weight, line)
+      cv2.putText(source, str(number), (int (x1)+45, int (y1)+45), cv2.FONT_ITALIC, 4, 255)
+      number +=1
+plt.imshow(source)
+plt.show()
