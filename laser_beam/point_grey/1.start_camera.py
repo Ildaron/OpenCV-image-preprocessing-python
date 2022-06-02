06ff9ab2641208a2a0bbbc22bad5c25c69ba15a0
Ildaron <de.ikar@yandex.ru>
2022-06-02 14:07:39 +0100

    Create 1.start_camera.py

---
+++ laser_beam/point_grey/1.start_camera.py
@@ -0,0 +1,28 @@
+from pyspin import PySpin
+import cv2
+def acquire_and_display_images(cam):
+        cam.BeginAcquisition()        
+        while(True):
+         image_result = cam.GetNextImage(1000)              
+         image_data = image_result.GetNDArray()
+         resize=50                    
+         x = int(image_data.shape[1]*resize/100)
+         y = int(image_data.shape[0]*resize/100) 
+         image_data=img=cv2.resize(image_data,(x,y))
+         cv2.imshow("img",image_data)
+         cv2.waitKey(1)                    
+        cam.EndAcquisition()
+def run_single_camera(cam):
+        result = True
+        cam.Init()                                                           # Initialize camera
+        nodemap = cam.GetNodeMap()                                           # Retrieve GenICam nodemap
+        result &= acquire_and_display_images(cam) # Acquire images       
+def main():
+    result =   True
+    system =   PySpin.System.GetInstance()     # Retrieve singleton reference to system object
+    cam_list = system.GetCameras()
+    num_cameras = cam_list.GetSize()
+    for i, cam in enumerate(cam_list):       # Run example on each camera
+        result &= run_single_camera(cam)
+    return result
+main()
