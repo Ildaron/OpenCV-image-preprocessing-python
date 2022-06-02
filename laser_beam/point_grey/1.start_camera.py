2d29773ca6027c7abe9c4f5aed973a831e32f044
Ildaron <ildar.o2010@yandex.ru>
2022-06-02 15:26:14 +0100

    Update 1.start_camera.py

--- laser_beam/point_grey/1.start_camera.py
+++ laser_beam/point_grey/1.start_camera.py
@@ -1,14 +1,18 @@
 from pyspin import PySpin
 import cv2
 def acquire_and_display_images(cam):
+        cam.PixelFormat.SetValue(PySpin.PixelFormat_RGB8)# for color    
         cam.BeginAcquisition()        
         while(True):
          image_result = cam.GetNextImage(1000)              
-         image_data = image_result.GetNDArray()
+         image_data = image_result.GetNDArray()#
          resize=50                    
          x = int(image_data.shape[1]*resize/100)
          y = int(image_data.shape[0]*resize/100) 
          image_data=img=cv2.resize(image_data,(x,y))
+         cv2.imwrite('C:/Users/Desktop/camera/Aperture/image2.jpg',image_data)
+         break
+         #image_data = cv2.cvtColor(image_data, cv2.COLOR_BGR2HSV)
          cv2.imshow("img",image_data)
          cv2.waitKey(1)                    
         cam.EndAcquisition()
