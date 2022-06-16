94859c552565d0932ddcf6a9f3b5f710bc82b5cb
Ildaron <ildar.o2010@yandex.ru>
2022-06-16 13:16:06 +0100

    Update 1.one_camera.py

--- laser_beam/FLIR/1.one_camera.py
+++ laser_beam/FLIR/1.one_camera.py
@@ -1,26 +1,28 @@
 import cv2
 from pyspin import PySpin
-def acquire_and_display_images(cam, nodemap, nodemap_tldevice):
-    node_acquisition_mode = PySpin.CEnumerationPtr(nodemap.GetNode('AcquisitionMode'))
-    node_acquisition_mode_continuous = node_acquisition_mode.GetEntryByName('Continuous')
-    acquisition_mode_continuous = node_acquisition_mode_continuous.GetValue()
+def acquire_and_display_images(cam):
+    #cam.PixelFormat.SetValue(PySpin.PixelFormat_RGB8)
     cam.BeginAcquisition()
-
     while(1):
             image_result = cam.GetNextImage(1000)                
             image_data = image_result.GetNDArray()
+            resize=25                    
+            x = int(image_data.shape[1]*resize/100)
+            y = int(image_data.shape[0]*resize/100) 
+            image_data=img=cv2.resize(image_data,(x,y))
+            #cv2.imwrite('C:/Users/ir2007/Desktop/HW/2.Camera/image3.jpg',image_data)     
+            #break
             cv2.imshow("img",image_data)
             cv2.waitKey(1)
             image_result.Release()
-            break
     cam.EndAcquisition()
-    return True     
+   
 def run_single_camera(cam):
         result = True
-        nodemap_tldevice = cam.GetTLDeviceNodeMap()
+       
         cam.Init()
         nodemap = cam.GetNodeMap()          # Retrieve GenICam nodemap
-        result &= acquire_and_display_images(cam, nodemap, nodemap_tldevice) # Acquire images
+        acquire_and_display_images(cam) # Acquire images
 def main():
     result = True
     system = PySpin.System.GetInstance()
@@ -28,5 +30,5 @@ def main():
     cam_list = system.GetCameras()
     num_cameras = cam_list.GetSize()
     for i, cam in enumerate(cam_list):
-        result &= run_single_camera(cam)
+        run_single_camera(cam)
 main()
